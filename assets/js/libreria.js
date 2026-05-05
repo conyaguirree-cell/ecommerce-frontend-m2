c98c4f9609ffd51a8a11707ddccd25abf19d7154
Constanza Aguirre Jimenez <cony.aguirree@gmail.comm>
2026-05-05 14:03:58 -0400

    pagina final

--- assets/js/libreria.js
+++ assets/js/libreria.js
@@ -1,4 +1,13 @@
-$("button").click(function(){ 
+// esta funcion esta asociada a un boton en especifico y deberia agregar boton por boton, se necesita un id para el boton 
+
+ /*$("#boton1").click(function(){ 
     var numeroproducto = parseInt($("#numeroproducto").html()) +1; 
 $("#numeroproducto").html(numeroproducto);
-});
+});*/ 
+
+
+// no esta asociada a nigun elemento de la pagina, se puede llamar desde cualquier elemento de la pagina 
+function addproducto() {
+    var numeroproducto = parseInt($("#numeroproducto").html()) + 1;
+    $("#numeroproducto").html(numeroproducto);
+}
